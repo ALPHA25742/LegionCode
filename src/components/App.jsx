5bdea79f9c217266bd1f2e4c48490c29f61b8938
ALPHA25742 <160420737082@mjcollege.ac.in>
2023-09-04 17:24:31 +0530

    removed ping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,9 @@ import Dashboard from "./Dashboard.jsx";
 import { ContactsProvider } from "../contexts/ContactsProvider.jsx";
 import { ConversationsProvider } from "../contexts/ConversationsProvider.jsx";
 import { SocketProvider } from "../contexts/SocketProvider.jsx";
-import { useEffect } from "react";
 
 function App() {
   const [id, setId] = useLocalStorage("id");
-  useEffect(()=>{
-    const pingServer = async () => {
-      const res = await fetch("https://legioncode-backend.onrender.com");
-      // const res = await fetch("http://localhost:3000/ping");
-      const data = await res.json();
-      console.log(data);
-    };
-    pingServer();
-  },[])
   const dashboard = (
     <SocketProvider id={id}>
     <ContactsProvider>
